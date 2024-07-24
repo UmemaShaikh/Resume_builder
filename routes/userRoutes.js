eb221bdc6a7d6e27b92278550bb2b90a64ad1809
UmemaShaikh <163534115+UmemaShaikh@users.noreply.github.com>
2024-07-24 03:31:09 +0000

    commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const user=require('../models/userModel')
+const app=express.Router();
+
+app.get('/login',async(req,res) =>{
+    try{
+       const result= await User.findOne({username:req.body.username, password:req.body.password})
+       if(result.length){
+        res.send('Login successful')
+       }
+
+    }catch(error){
+    res.status(400).json(error)
+    }
+
+})
+
+app.get('/register',async(req,res) =>{
+    try{
+       const newuser=new User(req.body)
+       await newuser.save()
+        res.send('Registration successful')
+    }catch(error){
+    res.status(400).json(error)
+    }
+
+})
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express= require('express')
 const app= express()
 const dbConnect = require('./dbConnect');
-
-
+app.use(express.json())
 const port =  process.env.PORT ||5000
+const userRoute= require('./routes/userRoutes')
+app.use('/api/user/',userRoute)
+
 
 app.get('/',(req,res)=>res.send('hello world'));
 
